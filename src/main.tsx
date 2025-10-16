6bbf181d5a286988b3c66bbaa69253fcc2322c7f
Fixer <nikita59856@gmail.com>
2025-10-16 18:32:48 +0300

    feat(analytics) add vercel analytics and speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css';
 import App from './App';
 import StarryBackground from './components/StarryBackground';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <StarryBackground></StarryBackground>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Analytics />
+        <SpeedInsights />
+        <StarryBackground></StarryBackground>
+        <App />
+    </StrictMode>,
 );
